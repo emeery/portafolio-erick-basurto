c62f9eabb59b775907739df697fb5e733be0e530
emeery <gerardoemery@live.com.mx>
2024-11-21 22:28:43 -0600

    primer config material y dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portafolio-erick';
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+  
 }
---
+++ src/shared/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+const AllMaterialModules = [
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatListModule,
+    MatIconModule
+];
+
+@NgModule({
+  imports: [AllMaterialModules],
+  exports: [AllMaterialModules],
+})
+export class MaterialModule {}
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
